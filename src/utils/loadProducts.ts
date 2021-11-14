e3d3e973ad76d44b234370e700a53f9fcb41b3c2
itsaadarsh <aadarsh.s@trell.in>
2021-11-14 17:07:39 +0530

    Adding more prodicts

--- src/utils/loadProducts.ts
+++ src/utils/loadProducts.ts
@@ -20,17 +20,45 @@ const products = [
   },
   {
     name: 'Bang $4',
-    tags: [
-      'Buzzcut',
-      'Headphones',
-      'Round Glasses',
-      'Bare Chest',
-      'Athletic Shorts',
-      'Basketball',
-      'Gold Chain',
-    ],
+    tags: ['Buzzcut', 'Headphones', 'Round Glasses', 'Bare Chest', 'Basketball', 'Gold Chain'],
     price: 90,
     image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=13&type=full',
+  },
+  {
+    name: 'Bang $5',
+    tags: ['Blonde', 'Halter Top', 'Skirt', 'Skater'],
+    price: 70,
+    image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=6&type=full',
+  },
+  {
+    name: 'Bang $6',
+    tags: ['Bald', 'Gray', 'Snoutz Tee', 'Purple', 'Black'],
+    price: 30,
+    image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=45&type=full',
+  },
+  {
+    name: 'Bang $7',
+    tags: ['Spiky', 'Full', 'Specs', 'Hoodie', 'Jeans', 'Tops'],
+    price: 65,
+    image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=48&type=full',
+  },
+  {
+    name: 'Bang $8',
+    tags: ['Fade', 'Logo Tee', 'Ripped Jeans', 'Workboots'],
+    price: 10,
+    image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=33&type=full',
+  },
+  {
+    name: 'Bang $9',
+    tags: ['Long', 'Halter Top', 'Camo', 'Canvas'],
+    price: 50,
+    image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=12&type=full',
+  },
+  {
+    name: 'Bang $10',
+    tags: ['Buzzcut', 'Brimmed', 'Tee', 'Dark Gray', 'Skater'],
+    price: 20,
+    image: 'https://meebits.larvalabs.com/meebitimages/characterimage?index=14&type=full',
   },
 ];
 
