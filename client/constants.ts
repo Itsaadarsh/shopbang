392091ba3cb2e84343e3d96a9577195250906704
itsaadarsh <aadarsh.s@trell.in>
2021-11-15 00:24:41 +0530

    final prod commit

--- client/constants.ts
+++ client/constants.ts
@@ -1,6 +1,6 @@
 export const config: { api: string; frontend: string } = {
   api: 'https://shopbang.herokuapp.com/api/v1',
-  frontend: 'https://localhost:3000/',
+  frontend: 'https://shopbang.vercel.app',
 };
 
 // export config;
