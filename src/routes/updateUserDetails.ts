4ef94f04d8b74b3c1ae2ef98664d3eec048e4e34
itsaadarsh <aadarsh.s@trell.in>
2021-11-15 00:07:19 +0530

    Changing update details

--- src/routes/updateUserDetails.ts
+++ src/routes/updateUserDetails.ts
@@ -19,20 +19,28 @@ router.patch('/user', auth, async (req: express.Request, res: express.Response) 
 
     for (let i = 0; i < key.length; i++) {
       if (key[i] == 'twitterId') {
-        user.twitterId = value[i];
-        tempBangToken += tokenValue.updateUserDetails;
+        if (value[i].length > 0) {
+          user.twitterId = value[i];
+          tempBangToken += tokenValue.updateUserDetails;
+        }
+        continue;
       } else if (key[i] == 'instagramId') {
-        user.instagramId = value[i];
-        tempBangToken += tokenValue.updateUserDetails;
+        if (value[i].length > 0) {
+          user.instagramId = value[i];
+          tempBangToken += tokenValue.updateUserDetails;
+        }
+        continue;
       } else if (key[i] == 'nationality') {
-        user.nationality = value[i];
-        tempBangToken += tokenValue.updateUserDetails;
+        if (value[i].length > 0) {
+          user.nationality = value[i];
+          tempBangToken += tokenValue.updateUserDetails;
+        }
+        continue;
       } else {
         res.status(400).json({ error: true, data: { message: [`Something went wrong, try again!`] } });
         return;
       }
     }
-
     user.bangToken = tempBangToken;
     const updatedUser = await user.save();
     return userResponse(res, updatedUser);
